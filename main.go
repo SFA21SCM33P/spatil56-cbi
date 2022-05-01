2545f512e56a60edf328aedcd2d18c29d911a092
SFA21SCM33P <spatil56@hawk.iit.edu>
2022-05-01 16:49:33 -0500

    Commit

--- main.go
+++ main.go
@@ -182,6 +182,11 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", port), nil))
 
 	// Test the database connection
 	//err = db.Ping()
@@ -200,12 +205,6 @@ func main() {
 		GetTaxiTrips(db)
 		GetUnemploymentRates(db)
 		GetBuildingPermits(db)
-
-		port := os.Getenv("PORT")
-		if port == "" {
-			port = "8080"
-		}
-		log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", port), nil))
 
 		// Pull the data once a day
 		// You might need to pull Taxi Trips and COVID data on daily basis
